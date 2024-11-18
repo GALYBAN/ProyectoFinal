e3610f122c251cc62a3b8eb541b076642d406bf9
GALYBAN <oscargalbae@gmail.com>
2024-11-18 13:52:37 +0100

    Relentizacion de movimiento en salto

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,6 +21,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float _sensorRadius = 0.5f;
     [SerializeField] LayerMask _groundLayer;
 
+    private string lastKey;
+
     void Awake()
     {
         _controller = GetComponent<CharacterController>();
@@ -35,11 +37,28 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _horizontal = Input.GetAxis("Horizontal");
+        _horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (!IsGrounded())
+        {
+            _speed = 2.5f;
+        }
+        else
+        {
+            _speed = 5;
+        }
 
         if(Input.GetButtonDown("Jump") && IsGrounded())
         {
             Jump();
+            if(_horizontal < 0)
+            {
+                lastKey = "left";
+            }
+            else if(_horizontal > 0)
+            {
+                lastKey = "right";
+            }
         }    
 
         Movement();
@@ -51,7 +70,7 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 direction = new Vector3(_horizontal, 0, 0);
 
-        if(direction != Vector3.zero)
+        if(direction != Vector3.zero && IsGrounded())
         {
             float targetAngle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
 
@@ -60,6 +79,23 @@ public class PlayerController : MonoBehaviour
             Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.right;
 
             _controller.Move(moveDirection * _speed * Time.deltaTime);
+        }
+        else if(direction != Vector3.zero && !IsGrounded())
+        {
+            if(lastKey == "left" && _horizontal > 0)
+            {
+                _speed = 2.5f;
+            }
+            else if(lastKey == "right" && _horizontal < 0)
+            {
+                _speed = 2.5f;
+            }
+            else
+            {
+                _speed = 5f;
+            }
+
+            _controller.Move(direction * _speed * Time.deltaTime);
         }
 
     }
